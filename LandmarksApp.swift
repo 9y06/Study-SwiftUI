5feaa6918d54ba0f1d00b39be3e560776e01b3f3
서진교 <164876484+9y06@users.noreply.github.com>
2024-09-27 21:23:53 +0900

    뷰 생성 및 결합

---
+++ LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by dgsw21 on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
